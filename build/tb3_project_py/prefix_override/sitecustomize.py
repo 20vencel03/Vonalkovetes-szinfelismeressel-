80575fe07f51a674bfec8bef1dee57a79fd5dd5c
20vencel03 <68946461+20vencel03@users.noreply.github.com>
2025-05-21 16:57:20 +0200

    Delete build directory

--- build/tb3_project_py/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/vencel/ros2_ws/src/Vonalkovetes-szinfelismeressel-/install/tb3_project_py'
